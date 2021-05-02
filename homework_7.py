85be4ecf94dd1650037e17506d328db9f1cd2908
Alexander Matsukevich <al.matsukevich@yandex.by>
2021-05-02 14:59:33 +0300

    8

--- homework_7.py
+++ homework_7.py
@@ -1,6 +1,34 @@
 import time
 import datetime
 import os
+import random
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+def color_print(lin_s, color):
+    if color == 1:
+        print(Fore.RED + lin_s)
+    if color == 2:
+        print(Fore.BLUE + lin_s)
+    if color == 3:
+        print(Fore.GREEN + lin_s)
+    if color == 4:
+        print(Fore.YELLOW + lin_s)
+    if color == 5:
+       print(Fore.WHITE + lin_s)
+
+def my_generator():
+    for n in range(1,10):
+        yield(n)
+
+def func_delimiter(del_pos):
+    g = next(del_pos)
+    if g > 8:
+        del_pos = my_generator()
+        g = next(del_pos)
+    return delimiter.get(g), del_pos
 
 num = {
     0 : [' ███████ ', ' ██   ██ ', ' ██   ██ ', ' ██   ██ ', ' ███████ '],
@@ -16,21 +44,17 @@ num = {
 }
 
 delimiter = {
-    1 : ['   ███   ', '         ', '   ███   ', '         ', '         '],
-    2 : ['   ███   ', '         ', '   ███   ', '         ', '         '],
-    3 : ['         ', '   ███   ', '         ', '   ███   ', '         '],
-    4 : ['         ', '   ███   ', '         ', '   ███   ', '         '],
-    5 : ['         ', '         ', '   ███   ', '         ', '   ███   '],
-    6 : ['         ', '         ', '   ███   ', '         ', '   ███   '],
-    7 : ['         ', '   ███   ', '         ', '   ███   ', '         '],
-    8 : ['         ', '   ███   ', '         ', '   ███   ', '         '],
+    1 : ['   ███   ', '         ', '         ', '         ', '         '],
+    2 : ['         ', '   ███   ', '         ', '         ', '         '],
+    3 : ['         ', '         ', '   ███   ', '         ', '         '],
+    4 : ['         ', '         ', '         ', '   ███   ', '         '],
+    5 : ['         ', '         ', '         ', '         ', '   ███   '],
+    6 : ['         ', '         ', '         ', '   ███   ', '         '],
+    7 : ['         ', '         ', '   ███   ', '         ', '         '],
+    8 : ['         ', '   ███   ', '         ', '         ', '         '],
 }
 
-def my_generator():
-    for n in range(1,10):
-        yield(n)
-
-ert = my_generator()
+del_position = my_generator()
 while True:
     time_now = str(datetime.datetime.now(tz=None).strftime("%H%M%S"))
     h1 = num.get(int(time_now[0]))
@@ -40,17 +64,15 @@ while True:
     s1 = num.get(int(time_now[4]))
     s2 = num.get(int(time_now[5]))
 
-    g = next(ert)
-    if g > 8:
-        ert = my_generator()
-        g = next(ert)
-
-    del_list = delimiter.get(int(g))
+    del_list, del_position = func_delimiter(del_position)
+    color = random.randint(1, 5)
 
     i = 0    
     while i <= 4:
-        lin_s = h1[i] + h2[i] + del_list[i] + m1[i] + m2[i] + del_list[i] + s1[i] + s2[i]
-        print(lin_s)
+        lin_s = h1[i] + h2[i] + del_list[i] + m1[i] + m2[i] + del_list[i] + \
+            s1[i] + s2[i]
+       # print(Fore.RED + lin_s)
+        color_print(lin_s, color)
         i += 1
-    time.sleep(0.4)
+    time.sleep(0.3)
     os.system('cls')
