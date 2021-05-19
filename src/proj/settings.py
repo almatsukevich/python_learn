85f189d5bfa97623feb1bc14f4f4cc0ed1b3bf6e
Alexander Matsukevich <al.matsukevich@yandex.by>
2021-05-19 20:35:16 +0300

    9

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xym#d&bas$(4v1%78fd)%oyy7h305u!i_i0u!fh#p&2by=8dt(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matsukevich.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
