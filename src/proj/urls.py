cc525a0cd85460e75c39b2baf82d11b2e75922cd
Alexander Matsukevich <al.matsukevich@yandex.by>
2021-05-20 17:50:35 +0300

    10

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from cities import views as cities_views
+
 urlpatterns = [
+    path('', cities_views.cities_home),
     path('admin/', admin.site.urls),
+    path('<str:code>/', cities_views.cities)
 ]
